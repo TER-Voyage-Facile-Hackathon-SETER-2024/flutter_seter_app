3b4d5141dc746f85f19d9c8a55784dc5108c7589
abda-gaye <lamineg049@gmail.com>
2024-05-04 10:53:50 +0000

    modification

--- lib/configs/config.dart
+++ lib/configs/config.dart
@@ -1,5 +1,7 @@
 class Config {
-  static const String apiUrl = "http://10.0.2.2:8000";
+  
+  // static const String apiUrl = "http://10.0.2.2:8000";
+  static const String apiUrl = "http://192.168.1.72:8000";
   static const String apiUrlSeter = "https://api-hackathon.seter.sn/api";
   //android 10.0.2.2
   //127.0.0.1:8000/api/v1
